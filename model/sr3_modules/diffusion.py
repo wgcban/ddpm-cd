07fa05d46ed95e62615e601680aef2e91227a921
wgcban <w.chamindabandara@gmail.com>
2022-04-10 21:06:49 -0400

    update to loss function

--- model/sr3_modules/diffusion.py
+++ model/sr3_modules/diffusion.py
@@ -216,6 +216,12 @@ class GaussianDiffusion(nn.Module):
             continuous_sqrt_alpha_cumprod * x_start +
             (1 - continuous_sqrt_alpha_cumprod**2).sqrt() * noise
         )
+    
+    def get_denoised_hsi(self, x_noisy, continuous_sqrt_alpha_cumprod, noise_recon):
+        # random gama
+        return (
+            (x_noisy-(1-continuous_sqrt_alpha_cumprod**2).sqrt()*noise_recon)/continuous_sqrt_alpha_cumprod
+        )
 
     def p_losses(self, x_in, noise=None):
         if self.model_hsi:
@@ -241,9 +247,13 @@ class GaussianDiffusion(nn.Module):
         x_noisy = self.q_sample(
             x_start=x_start, continuous_sqrt_alpha_cumprod=continuous_sqrt_alpha_cumprod.view(-1, 1, 1, 1), noise=noise)
 
-        x_recon = self.denoise_fn(x_noisy, x_in['P'], x_in['SR'], continuous_sqrt_alpha_cumprod)
+        noise_recon = self.denoise_fn(x_noisy, x_in['P'], x_in['SR'], continuous_sqrt_alpha_cumprod)
 
-        loss = self.loss_func(noise, x_recon)
+        #Comment out these two lines
+        x_recon = self.get_denoised_hsi(x_noisy, continuous_sqrt_alpha_cumprod.view(-1, 1, 1, 1), noise_recon)
+        loss    = self.loss_func(x_start, x_recon)
+        
+        #loss = self.loss_func(noise, noise_recon)
         return loss
 
     def forward(self, x, *args, **kwargs):
